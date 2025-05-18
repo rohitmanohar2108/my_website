914b0936a75519362f61a23d4cf2a6703c096517
Rohit Manohar <manoharrohit372@gmail.com>
2025-05-19 03:00:02 +0530

    j

--- src/components/ui/google-gemini-effect.tsx
+++ src/components/ui/google-gemini-effect.tsx
@@ -24,7 +24,7 @@ export const GoogleGeminiEffect = ({
       <p className="text-3xl md:text-5xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
         {title || `Want to become world's top 1% software Developer`}
       </p>
-      <p className="text-lg md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
+      <p className="text-sm md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
         {description ||
           `Scroll to explore a blend of design, interactivity,
        and dynamic visuals!`}
